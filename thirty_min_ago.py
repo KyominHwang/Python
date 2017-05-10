5e52ab352cace498bc3ff642d13f09b474d7c274
Kyomin Hwang <piter0208@kookmin.ac.kr>
2017-05-11 00:34:14 +0900

    thirty_min_ago

---
+++ thirty_min_ago.py
@@ -0,0 +1,14 @@
+#수호는 30분전으로 돌아가고 싶은 1인이다.
+#공백을 기준으로 시간 분이 입력된다
+#그러면 이 시간을 기준으로 30분 전의 시간을 출력하시오!
+hour = int(input('몇시인지 입력하세요!'))
+minu = int(input('몇 분인지 입력하세요!'))
+def cal_time(a,b) :
+    if b - 30 < 0 :
+        a = a - 1
+        b = b  + 30
+    else :
+         b = b - 30
+    return a,b
+
+print(cal_time(hour,minu))
