a48aa7a5a9abe1d16babe800ae9c218e128a93fa
KyominHwang <piter0208@kookmin.ac.kr>
2019-11-11 20:55:14 +0900

    Create conditional_statement.py

---
+++ conditional_statement.py
@@ -0,0 +1,36 @@
+#파이썬 흐름제어(제어문)
+#조건문 실습
+print(type(True))
+print(type(False))
+
+#ex1
+if True :
+  print("Yes")
+  
+  
+#ex2
+if False:
+  print("No")
+  
+  
+#ex3
+if False :
+  print("No")
+else:
+  print("yes")
+
+#관계연산자
+#>,>=,<,<=,==,!=
+
+#참 거짓 종류
+#참 : "내용",[내용],(내용),{내용},1
+#거짓 : "",[],(),{},0
+city = ''
+if city:
+  print("Ture")
+else:
+  print("False")
+#Flase 출력
+
+#논리연산자
+#and or not
